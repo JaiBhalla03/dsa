3786a8d8714eda1c3495d4ef2ca7d35223048fa4
jai <jai.bhalla2021@vitstudent.ac.in>
2023-10-10 23:02:07 +0530

    more codes

---
+++ divisible_and_non_divisible_sum_difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 1 ; i <= n ; i++){
+            if(i%m == 0){
+                sum2 += i;
+            }
+            else{
+                sum1+=i;
+            }
+        }
+        return sum1 - sum2;
+    }
+};
