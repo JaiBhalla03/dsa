89095bcd2a59151d598c23184da97a6097d756ae
JaiBhalla03 <jai.bhalla2021@vitstudent.ac.in>
2024-03-27 19:58:34 +0530

    more codes

--- sdot_practice/prog27.java
+++ sdot_practice/prog27.java
@@ -15,4 +15,35 @@ Output:-
 * */
 
 public class prog27 {
+    public static int ans = 0;
+    public static int dfs(int i, List<List<Integer>> adj, int seats, int[] vis){
+        vis[i] = 1;
+        int persons = 1;
+        for(int it : adj.get(i)){
+            if(vis[it] == 0){
+                persons += dfs(it, adj, seats, vis);
+            }
+        }
+        if(i != 0){
+            ans += Math.ceil((double)persons/seats);
+        }
+        return persons;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] edges = sc.nextLine().split(",");
+        int V = edges.length + 1;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < V ; i++) adj.add(new ArrayList<>());
+        for(String edge : edges){
+            int u = Integer.parseInt(Character.toString(edge.charAt(0)));
+            int v = Integer.parseInt(Character.toString(edge.charAt(edge.length() - 1)));
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int seats = sc.nextInt();
+        int[] vis = new int[V];
+        dfs(0, adj, seats, vis);
+        System.out.println(ans);
+    }
 }
--- sdot_practice/prog28.java
+++ sdot_practice/prog28.java
@@ -22,5 +22,37 @@ Output:-
 */
 
 public class prog28 {
-
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < V ; i++) adj.add(new ArrayList<>());
+        for(int i = 0 ; i < E ; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+        }
+        int[] indegree = new int[V];
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> topo = new ArrayList<>();
+        for(int i = 0 ; i < V ; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        for(int i = 0 ; i < V ; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+        while(!q.isEmpty()){
+            int node = q.poll();
+            topo.add(node);
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+        boolean ans = topo.size() == V;
+        System.out.println(ans);
+    }
 }
