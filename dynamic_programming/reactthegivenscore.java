bb40978c433723d9a9ab6d18aa4b654c84bc6361
JaiBhalla03 <jai.bhalla2021@vitstudent.ac.ingit config --global user.name JaiBhalla03:git config --global user.email jai.bhalla2021@vitstudent.ac.in>
2024-04-11 19:24:00 +0530

    more codes

---
+++ dynamic_programming/reactthegivenscore.java
@@ -0,0 +1,16 @@
+class Geeks {
+    public long count(int n) {
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        for(int i = 3 ; i <= n ; i++){
+            dp[i] += dp[i-3];
+        }
+        for(int i = 5 ; i <= n ; i++){
+            dp[i] += dp[i-5];
+        }
+        for(int i = 10 ; i <= n ; i++){
+            dp[i] += dp[i-10];
+        }
+        return dp[n];
+    }
+}
