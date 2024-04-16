5c15be5ade65f4f6136b7e73e3210c0a594c8acd
JaiBhalla03 <jai.bhalla2021@vitstudent.ac.ingit config --global user.name JaiBhalla03:git config --global user.email jai.bhalla2021@vitstudent.ac.in>
2024-04-16 19:39:03 +0530

    more codes

--- dynamic_programming/knapsackWithDuplicateItems.java
+++ dynamic_programming/knapsackWithDuplicateItems.java
@@ -1,2 +1,26 @@
-public class knapsackWithDuplicateItems {
+class Solution{
+    static int helper(int index, int W, int[] val, int[] wt, int[][] dp){
+        if(index == 0){
+            int a = W/wt[0];
+            return val[0]*a;
+        }
+        if(dp[index][W] != -1){
+            return dp[index][W];
+        }
+        int notTake = helper(index - 1, W, val, wt, dp);
+        int take = 0;
+        if(wt[index] <= W){
+            take = val[index] + helper(index, W - wt[index], val, wt, dp);
+        }
+        return dp[index][W] = Math.max(take, notTake);
+    }
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        int[][] dp = new int[N+1][W+1];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
+        }
+        int ans = helper(N-1, W, val, wt, dp);
+        return ans;
+    }
 }
